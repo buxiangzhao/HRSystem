e7c5400ffd1fb672b8b60560aec3503144c702b9
Buxiangzhao <SQ_BXZ@windows10.microdone.cn>
2017-03-24 17:43:10 +0800

    AspectJ--测试使用

---
+++ src/com/aspect/AuthAspectj.java
@@ -0,0 +1,14 @@
+package com.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+
+@Aspect
+public class AuthAspectj {
+//	@Before
+	private void sdd() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
